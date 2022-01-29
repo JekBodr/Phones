113960f39689cd9debcb5d2de796999a611a9452
JekBodr <jekbodr@gmail.com>
2022-01-29 13:36:51 +0200

    add database

--- src/app/App.java
+++ src/app/App.java
@@ -1,18 +1,10 @@
 package app;
 
-import app.controllers.AppController;
-import app.models.AppModel;
-import app.views.AppView;
-
-
+import app.utils.AppStarter;
 
 public class App {
      //Вход в программу
-    public static void main(String[] args) {
-
-        AppModel model = new AppModel();
-        AppView view = new AppView(model);
-        AppController controller = new AppController(model, view);
-        controller.runApp();
-    }
+     public static void main(String[] args) {
+         AppStarter.startApp();
+     }
 }
